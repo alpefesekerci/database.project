9a2819562d5cb787ba9b682b7f1999d7df192cb1
Alp Efe Şekerci <alpefesekerci@gmail.com>
2026-03-11 10:31:02 +0300

    Scanner added

--- src/DbConnection.java
+++ src/DbConnection.java
@@ -1,8 +1,11 @@
 public class DbConnection {
     public void openConnection(){
+        System.out.println("Connection Opened");
     }
     public void closeConnection(){
+        System.out.println("Connection Closed");
     }
     public void executeQuery(String query){
+        System.out.println("Query Executed");
     }
 }
--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,34 @@
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
-        DbConnection dbConnection = new OracleDbConnection();
-        dbConnection.openConnection();
-        dbConnection.executeQuery("SELECT * FROM student");
-        dbConnection.closeConnection();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Database Type (1 for MySQL, 2 for MsSQL, 3 for Oracle): ");
+        int dbType = scanner.nextInt();
+
+        DbConnection dbConnection = null;
+
+        switch (dbType) {
+            case 1:
+                dbConnection = new MySQLConnection();
+                break;
+            case 2:
+                dbConnection = new MsSQLConnection();
+                break;
+            case 3:
+                dbConnection = new OracleDbConnection();
+                break;
+            default:
+                System.out.println("Invalid database type.");
+        }
+        if (dbConnection != null) {
+            dbConnection.openConnection();
+            dbConnection.executeQuery("SELECT * FROM users");
+            dbConnection.closeConnection();
+            System.out.println("Operation completed successfully.");
+            scanner.close();
+            System.exit(0);
+        }
     }
 }
